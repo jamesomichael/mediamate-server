bc3253f092a213c949115130502358a3051d1cea
James Michael <jomichael1994@gmail.com>
2025-02-05 22:48:06 +0000

    implement range requests for media playback

--- controllers/play.js
+++ controllers/play.js
@@ -8,9 +8,41 @@ const playMedia = async (req, res) => {
 	const filePath = `${MEDIA_DIR}/${type}/${fileName}`;
 	const stat = fs.statSync(filePath);
 	const fileSize = stat.size;
+
+	const range = req.headers.range;
+
+	let fileStream;
+
+	if (range) {
+		const [start, end] = range
+			.replace(/bytes=/, '')
+			.split('-')
+			.map(Number);
+
+		const chunkStart = start || 0;
+		const chunkEnd =
+			end || Math.min(chunkStart + 1024 * 1024, fileSize - 1);
+
+		if (chunkStart >= fileSize) {
+			return res.status(416).send('Requested range not satisfiable');
+		}
+
+		fileStream = fs.createReadStream(filePath, {
+			start: chunkStart,
+			end: chunkEnd,
+		});
+		res.status(206);
+		res.setHeader(
+			'Content-Range',
+			`bytes ${chunkStart}-${chunkEnd}/${fileSize}`
+		);
+		res.setHeader('Accept-Ranges', 'bytes');
+		res.setHeader('Content-Length', chunkEnd - chunkStart + 1);
+	} else {
+		res.setHeader('Content-Length', fileSize);
+		fileStream = fs.createReadStream(filePath);
+	}
 	res.setHeader('Content-Type', 'video/mp4');
-	res.setHeader('Content-Length', fileSize);
-	const fileStream = fs.createReadStream(filePath);
 	fileStream.pipe(res);
 };
 
